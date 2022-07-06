e33ab6a8e81ce4c645c60b63cc3917d1d648bf28
Digoh-Oliveira <digoh.psn@hotmail.com>
2022-07-06 17:18:31 -0300

    sexto commit

--- js/game.js
+++ js/game.js
@@ -1,8 +1,10 @@
 const grid = document.querySelector('.grid');
 
+/* Array */
 const gameChars = [ 'beth','jerry','jessica','morty','pessoa-passaro',
 'pickle-rick','rick','summer','meeseeks','scroopy',];
 
+/* Função responsável por criar as Divs */
 const createElement = (tag, className) => {
 
     const element = document.createElement(tag);
@@ -10,6 +12,67 @@ const createElement = (tag, className) => {
     
     return element;}
 
+
+let firstCard = '';
+let secondCard = '';
+
+const checkEndGame = () =>{
+    const disableCards = document.querySelectorAll('.disabled-card');
+
+    if(disableCards.length == 20){
+        alert('Parabéns, você conseguiu!');
+    }
+}
+
+const checkCards = () => {
+
+    const firstCharacter = firstCard.getAttribute('data-character');
+    const secondCharacter = secondCard.getAttribute('data-character');
+
+    if(firstCharacter == secondCharacter){
+
+        firstCard.firstChild.classList.add('disabled-card');
+        secondCard.firstChild.classList.add('disabled-card');
+
+        firstCard = '';
+        secondCard = '';
+
+        checkEndGame();
+    
+    }else{
+
+        setTimeout(() => {
+            
+            firstCard.classList.remove('reveal-card');
+            secondCard.classList.remove('reveal-card');
+
+            firstCard = '';
+            secondCard = '';
+    
+        }, 500);
+        
+    }
+}
+
+const revealCard = ({target}) => {
+    
+    if (target.parentNode.className.includes('reveal-card')){
+        return;
+    }
+    
+    if (firstCard == ''){
+        target.parentNode.classList.add('reveal-card');
+        firstCard = target.parentNode;
+    
+    }else if(secondCard == ''){
+        target.parentNode.classList.add('reveal-card');
+        secondCard = target.parentNode;
+
+        checkCards();
+    }
+}
+    
+/* Função responsável por criar as cartas e adicionar as imagens dos personagens */
 const createCard = (character) => {
 
     const card = createElement('div', 'card');
@@ -20,9 +83,12 @@ const createCard = (character) => {
 
     card.appendChild(front);
     card.appendChild(back);
+    card.setAttribute('data-character', character);
+    card.addEventListener('click', revealCard);
 
     return card;}
 
+    /* Função responsável por carregar as cartas com os personagens aleatorios */
 const loadGame = () =>{
 
     const duplicateChars = [...gameChars, ...gameChars];
